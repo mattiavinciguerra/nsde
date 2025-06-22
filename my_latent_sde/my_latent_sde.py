4c82440c34c2b973837302bc91553b386dc11183
mattiavinciguerra <mattia17b@gmail.com>
2025-06-22 15:48:11 +0200

    device cpu

--- my_latent_sde/my_latent_sde.py
+++ my_latent_sde/my_latent_sde.py
@@ -189,7 +189,8 @@ input_size = 2 # Coppie di coordinate
 hidden_size = 128 # Dimensione dello stato nascosto
 batch_size = 16 # Dimensione del batch
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+#device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+device = torch.device('cpu') # test
 print("Using device:", device)
 
 num_epochs = 200
